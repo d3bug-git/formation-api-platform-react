a7146e32bdcc70dc3a926b528bb963843f365390
serge watchou <swatchou@yahoo.com>
2019-11-09 14:50:59 +0100

    Installation de l'API platform et mise en place de nos ressources

---
+++ src/Entity/Customer.php
@@ -0,0 +1,202 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use ApiPlatform\Core\Annotation\ApiFilter;
+use Doctrine\Common\Collections\Collection;
+use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Annotation\ApiSubresource;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Serializer\Annotation\Groups;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CustomerRepository")
+ * @ApiResource(
+ * subresourceOperations={
+ * "invoices_get_subresource"={"path"="/customers/{id}/invoices"}
+ * },
+ * collectionOperations={"GET","POST"},
+ * itemOperations={"GET","PUT","DELETE"},
+ * normalizationContext={
+ *  "groups"={"customers_read"}
+ * }
+ * )
+ * @ApiFilter(SearchFilter::class,properties={"firstName":"partial","lastName","company"})
+ */
+class Customer
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     * @Groups({"customers_read","invoices_read"})
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Groups({"customers_read","invoices_read"})
+     * @Assert\NotBlank(message="Le prénom du customer est obligatoire")
+     * @Assert\Length(min=3,minMessage="Le prénom doit faire entre 3 et 255 caractères",
+     * max=255,maxMessage="Le prénom doit faire entre 3 et 255 caractères")
+     */
+    private $firstName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Groups({"customers_read","invoices_read"})
+     * @Assert\NotBlank(message="Le nom du customer est obligatoire")
+     */
+    private $lastName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Groups({"customers_read","invoices_read"})
+     * @Assert\NotBlank(message="L'adresse email'du customer est obligatoire")
+     * @Assert\Email(message="Le format de l'adresse email est invalide")
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255,nullable=true)
+     * @Groups({"customers_read","invoices_read"})
+     */
+    private $company;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Invoice", mappedBy="customer")
+     * @Groups({"customers_read"})
+     * @ApiSubresource
+     */
+    private $invoices;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="customers")
+     * @Groups({"customers_read"})
+     * @Assert\NotBlank(message="L'utilisateur est obligatoire")
+     */
+    private $user;
+
+    public function __construct()
+    {
+        $this->invoices = new ArrayCollection();
+    }
+    
+    /**
+     * Permet de récupérer le total des invoices
+     *@Groups({"customers_read"})
+     * @return float
+     */
+    public function getTotalAmount(): float{
+        return array_reduce($this->invoices->toArray(),function($total,$invoice){
+            return $total+$invoice->getAmount();
+        },0);
+    }
+    /**
+     * @Groups({"customers_read"})
+     */
+    public function getUnpaidAmount():float{
+        return array_reduce($this->invoices->toArray(),function($total,$invoice){
+            return $total+ ($invoice->getStatus()==="PAID" ||$invoice->getStatus()==="CANCELLED" ? 0 : $invoice->getAmount());
+        },0);
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getCompany(): ?string
+    {
+        return $this->company;
+    }
+
+    public function setCompany(string $company): self
+    {
+        $this->company = $company;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Invoice[]
+     */
+    public function getInvoices(): Collection
+    {
+        return $this->invoices;
+    }
+
+    public function addInvoice(Invoice $invoice): self
+    {
+        if (!$this->invoices->contains($invoice)) {
+            $this->invoices[] = $invoice;
+            $invoice->setCustomer($this);
+        }
+
+        return $this;
+    }
+
+    public function removeInvoice(Invoice $invoice): self
+    {
+        if ($this->invoices->contains($invoice)) {
+            $this->invoices->removeElement($invoice);
+            // set the owning side to null (unless already changed)
+            if ($invoice->getCustomer() === $this) {
+                $invoice->setCustomer(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Invoice.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Serializer\Annotation\Groups;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\OrderFilter;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\InvoiceRepository")
+ * @ApiResource(
+ * subresourceOperations={
+ * "api_customers_invoices_get_subresource"={
+ * "normalization_context"={"groups"={"invoices_subresource"}}
+ * }
+ * },
+ * attributes={
+ *      "pagination_enabled"=true,
+ *      "order":{"amount":"desc"}
+ * },
+ * normalizationContext={
+ * "groups"={"invoices_read"}
+ * },
+ * denormalizationContext={"disable_type_enforcement":true}
+ * )
+ * @ApiFilter(OrderFilter::class,properties={"amount","sentAt"})
+ */
+class Invoice
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     * @Groups({"invoices_read","customers_read","invoices_subresource"})
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="float")
+     * @Groups({"invoices_read","customers_read","invoices_subresource"})
+     * @Assert\NotBlank(message="Le montant de la facture est obligatoire")
+     * @Assert\Type(type="numeric",message="Le montant de la facture doit être un numérique !")
+     */
+    private $amount;
+
+    /**
+     * @ORM\Column(type="datetime")
+     * @Groups({"invoices_read","customers_read"})
+     * @Assert\DateTime(message="La date doit être au format YYYY-MM-DD")
+     * @Assert\NotBlank(message="La date d'envoi doit être renseignée")
+     */
+    private $sentAt;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Groups({"invoices_read","customers_read","invoices_subresource"})
+     * @Assert\Choice(choices={"SENT","PAID","CANCELLED"},message="Le statut doit être SENT,PAID ou CANCELLED")
+     */
+    private $status;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Customer", inversedBy="invoices")
+     * @ORM\JoinColumn(nullable=false)
+     * @Groups({"invoices_read"})
+     * @Assert\NotBlank(message="Le client de la facture doit être renseigné")
+     */
+    private $customer;
+
+    /**
+     * @ORM\Column(type="integer")
+     * @Groups({"invoices_read","customers_read","invoices_subresource"})
+     * @Assert\NotBlank(message="Il faut absolument un chrono pour la facture")
+     * @Assert\Type(type="integer",message="Le chrono doit être un nombre")
+     */
+    private $chrono;
+    /**
+     * Recuperer l'User de la facture
+     *@Groups({"invoices_read","invoices_subresource"})
+     * @return User
+     */
+    public function getUser():User{
+        return $this->customer->getUser();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAmount(): ?float
+    {
+        return $this->amount;
+    }
+
+    public function setAmount($amount): self
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    public function getSentAt(): ?\DateTimeInterface
+    {
+        return $this->sentAt;
+    }
+
+    public function setSentAt($sentAt): self
+    {
+        $this->sentAt = $sentAt;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getCustomer(): ?Customer
+    {
+        return $this->customer;
+    }
+
+    public function setCustomer(?Customer $customer): self
+    {
+        $this->customer = $customer;
+
+        return $this;
+    }
+
+    public function getChrono(): ?int
+    {
+        return $this->chrono;
+    }
+
+    public function setChrono($chrono): self
+    {
+        $this->chrono = $chrono;
+
+        return $this;
+    }
+}

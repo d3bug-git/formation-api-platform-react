5915ee98a14472d36aa022cf16a97362ed6d5817
serge watchou <swatchou@yahoo.com>
2019-11-09 18:36:42 +0100

    Ajout de l'authentification via JWT

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -13,7 +13,9 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
- * @ApiResource
+ * @ApiResource(
+ * normalizationContext={"groups"={"users_read"}}
+ * )
  * @UniqueEntity("email",message="Un utilisateur ayant cette adresse email existe déja")
  */
 class User implements UserInterface
@@ -22,13 +24,13 @@ class User implements UserInterface
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
-     * @Groups({"invoices_read","invoices_subresource"})
+     * @Groups({"invoices_read","invoices_subresource","users_read"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=180, unique=true)
-     * @Groups({"invoices_read","invoices_subresource"})
+     * @Groups({"invoices_read","invoices_subresource","users_read"})
      * @Assert\NotBlank(message="L'email doit être renseigné")
      * @Assert\Email(message="Le format de l'adresse email n'est pas valide")
      */
@@ -48,14 +50,14 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Groups({"invoices_read","invoices_subresource"})
+     * @Groups({"invoices_read","invoices_subresource","users_read"})
      * @Assert\NotBlank(message="Le prenom doit être renseigné")
      */
     private $firstName;
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Groups({"invoices_read","invoices_subresource"})
+     * @Groups({"invoices_read","invoices_subresource","users_read"})
      * @Assert\NotBlank(message="Le nom doit être renseigné")
      */
     private $lastName;
